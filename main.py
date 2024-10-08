2b0cb52a6aab26ceab5b372bb0125da1b6b86244
Troy <tprebenda@gmail.com>
2024-10-08 17:58:52 -0400

    refactor leaf generation- more modular

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ CANDIDATE_ID = "95d446bf-5b0b-4805-bd71-d9e131343ba0"
 PHASE_1_GRID_SIZE = 10
 PHASE_2_GRID_SIZE = 30
 PHASE_2_GRID_CENTER = 13
+# (row, column)
+DIRECTION_MODIFIER = {
+    "up-right": (-1, 1),
+    "up-left": (-1, -1),
+    "down-right": (1, 1),
+    "down-left": (1, -1),
+}
 
 # Literals
 ASTRAL_ENTITY = Literal["polyanet", "soloon", "cometh"]
@@ -31,21 +38,22 @@ def create_polyanet_across() -> None:
 
 
 def create_crossmint_logo() -> None:
-    """Creates the Phase 2 grid in the shape of the Crossmint logo."""
-    # TODO
-    # planets are symmetrical over x and y axis
-    # 7 comets going left, 6 going the other directions
-    # 6 red moons, 7 purple/white, 8 blue
+    """Creates the Crossmint logo design for Phase 2 of the Challenge."""
 
-    # initial cross at center
+    # initial polyanet cross at center
     generate_entity(entity="polyanet", row=PHASE_2_GRID_CENTER, column=PHASE_2_GRID_CENTER)
     generate_entity(entity="polyanet", row=PHASE_2_GRID_CENTER - 1, column=PHASE_2_GRID_CENTER)
     generate_entity(entity="polyanet", row=PHASE_2_GRID_CENTER, column=PHASE_2_GRID_CENTER + 1)
     generate_entity(entity="polyanet", row=PHASE_2_GRID_CENTER + 1, column=PHASE_2_GRID_CENTER)
     generate_entity(entity="polyanet", row=PHASE_2_GRID_CENTER, column=PHASE_2_GRID_CENTER - 1)
 
+    # Add four 'leaves' of polyanets away from grid center
     for direction in get_args(DIAGONAL_DIRECTIONS):
-        generate_leaf_for_direction(direction)
+        generate_leaf_along_diagonal(direction)
+
+    # TODO:
+    # Add soloons
+    # Add comeths
 
 
 def generate_entity(
@@ -120,127 +128,53 @@ def delete_entity(
         print(f"DELETE request to '/{entity}s' failed: {response.text}")
 
 
-def generate_leaf_for_direction(direction: DIAGONAL_DIRECTIONS):
+def generate_leaf_along_diagonal(direction: DIAGONAL_DIRECTIONS) -> None:
+    """
+    Traces along the diagonals of the grid (starting at the center of the shape, row = 13, col = 13),
+    and adds polyanets to their correct location based on the offset from the diagonal.
+
+    Uses a row + column modifier (either 1 or -1) to account for each direction of the leaves.
+
+        Parameters:
+            direction: dirction to use for cometh creation. Must be one of \
+            ["up", "down", "left", "right"]
+
+        Returns:
+            None
+    """
     offsets = [1, 2, 3, 4, 3, 3, 2, 2, 1, 1]
     offset_idx = 0
-    diagonal_coordinate_generator = diagonal_coordinates_factory(direction)
+    row_modifier, col_modifier = DIRECTION_MODIFIER[direction]
     for i in range(1, 12):
-        (diagonal_r, diagonal_c) = diagonal_coordinate_generator(i)
-        # print(f"{diagonal_r}, {diagonal_c}")
+        diagonal_r = PHASE_2_GRID_CENTER + (i * row_modifier)
+        diagonal_c = PHASE_2_GRID_CENTER + (i * col_modifier)
         # final corner point
         if i == 11:
             generate_entity(entity="polyanet", row=diagonal_r, column=diagonal_c)
             break
 
-        coords = get_polyanet_cords_for_direction(diagonal_r, diagonal_c, offsets, offset_idx, direction)
-        # print(coords)
+        offset = offsets[offset_idx]
+        if offset_idx < 3:
+            coords = [
+                (diagonal_r + (offset * row_modifier), diagonal_c),
+                (diagonal_r + ((offset + 1) * row_modifier), diagonal_c),
+                (diagonal_r, diagonal_c + (offset * col_modifier)),
+                (diagonal_r, diagonal_c + ((offset + 1) * col_modifier)),
+            ]
+        else:
+            coords = [
+                (diagonal_r + (offset * row_modifier), diagonal_c),
+                (diagonal_r, diagonal_c + (offset * col_modifier)),
+            ]
+
         for row, col in coords:
             generate_entity(entity="polyanet", row=row, column=col)
         offset_idx += 1
 
 
-def get_polyanet_cords_for_direction(diagonal_r, diagonal_c, offsets, offset_idx, direction):
-    offset = offsets[offset_idx]
-    if direction == "up-right":
-        if offset_idx < 3:
-            coords = [
-                (diagonal_r - offset, diagonal_c),
-                (diagonal_r - offset - 1, diagonal_c),
-                (diagonal_r, diagonal_c + offset),
-                (diagonal_r, diagonal_c + offset + 1),
-            ]
-        else:
-            coords = [
-                (diagonal_r - offset, diagonal_c),
-                (diagonal_r, diagonal_c + offset),
-            ]
-    elif direction == "up-left":
-        if offset_idx < 3:
-            coords = [
-                (diagonal_r - offset, diagonal_c),
-                (diagonal_r - offset - 1, diagonal_c),
-                (diagonal_r, diagonal_c - offset),
-                (diagonal_r, diagonal_c - offset - 1),
-            ]
-        else:
-            coords = [(diagonal_r - offset, diagonal_c), (diagonal_r, diagonal_c - offset)]
-    elif direction == "down-right":
-        if offset_idx < 3:
-            coords = [
-                (diagonal_r + offset, diagonal_c),
-                (diagonal_r + offset + 1, diagonal_c),
-                (diagonal_r, diagonal_c + offset),
-                (diagonal_r, diagonal_c + offset + 1),
-            ]
-        else:
-            coords = [(diagonal_r + offset, diagonal_c), (diagonal_r, diagonal_c + offset)]
-    else:
-        if offset_idx < 3:
-            coords = [
-                (diagonal_r + offset, diagonal_c),
-                (diagonal_r + offset - 1, diagonal_c),
-                (diagonal_r, diagonal_c - offset),
-                (diagonal_r, diagonal_c - offset - 1),
-            ]
-        else:
-            coords = [(diagonal_r + offset, diagonal_c), (diagonal_r, diagonal_c - offset)]
-    return coords
-
-
-def get_up_right_diagonal_coords(idx: int):
-    return (PHASE_2_GRID_CENTER - idx, PHASE_2_GRID_CENTER + idx)
-
-
-def get_up_left_diagonal_coords(idx: int):
-    return (PHASE_2_GRID_CENTER - idx, PHASE_2_GRID_CENTER - idx)
-
-
-def get_down_right_diagonal_coords(idx: int):
-    return (PHASE_2_GRID_CENTER + idx, PHASE_2_GRID_CENTER + idx)
-
-
-def get_down_left_diagonal_coords(idx: int):
-    return (PHASE_2_GRID_CENTER + idx, PHASE_2_GRID_CENTER - idx)
-
-
-def diagonal_coordinates_factory(direction: DIAGONAL_DIRECTIONS):
-    diagonal_funcs = {
-        "up-right": get_up_right_diagonal_coords,
-        "up-left": get_up_left_diagonal_coords,
-        "down-right": get_down_right_diagonal_coords,
-        "down-left": get_down_left_diagonal_coords,
-    }
-    return diagonal_funcs[direction]
-
-
-# def test(diagonal_r, diagonal_c, offsets, offset_idx, direction):
-#     direction_modifier = {
-#         "up-right": (1, -1),
-#         "up-left": (-1, -1),
-#         "down-right": (1, 1),
-#         "down-left": (-1, 1),
-#     }
-#     offset = offsets[offset_idx]
-#     if direction == "up-right":
-#         row_modifier, col_modifier = direction_modifier(direction)
-#         if offset_idx < 3:
-#             coords = [
-#                 (diagonal_r + (offset * row_modifier), diagonal_c),
-#                 (diagonal_r + ((offset + 1) * row_modifier), diagonal_c),
-#                 (diagonal_r, diagonal_c + ((offset) * col_modifier)),
-#                 (diagonal_r, diagonal_c + ((offset + 1) * col_modifier)),
-#             ]
-#         else:
-#             coords = [
-#                 (diagonal_r + (offset * row_modifier), diagonal_c),
-#                 (diagonal_r, diagonal_c + (offset * col_modifier)),
-#             ]
-#     return coords
-
-
 def main() -> None:
     # Phase 1
-    # create_polyanet_across()
+    create_polyanet_across()
 
     # Phase 2
     create_crossmint_logo()
